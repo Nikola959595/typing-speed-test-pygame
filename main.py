7f70cf780c5d6a8ed21adc8b8889118e813d0438
Nikola959595 <mnikola088@gmail.com>
2024-05-30 23:18:26 +0200

    basic functionality

--- main.py
+++ main.py
@@ -1,20 +1,74 @@
-from tkinter import *
+import pygame
+import sys
+import random
 from words import words
 
+words_list = random.sample(words, 150)
+words_to_type = " ".join(words_list)
+letters_to_type = list(words_to_type)
+print(letters_to_type)
+typed_letters = []
 
-root = Tk()
-root.title("Typing speed test")
-root.geometry("800x500")
+pygame.init()
+screen = pygame.display.set_mode((900, 500))
+text_font = pygame.font.SysFont("Arial", 50)
+pygame.display.set_caption("Typing Speed Test")
+bg = pygame.image.load('background.png')
+clock = pygame.time.Clock()
+running = True
 
-bg = PhotoImage(file = "background.png")
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x,y))
+    return img.get_width()
+    
 
-canvas = Canvas(width=800,height=500)
-canvas.pack(fill = 'both', expand=True)
-canvas.create_image(0, 0, image = bg, anchor = "nw")
+start_text_display = True
+
+letter_pos_x = 350
+letter_pos_y = 250
+letter_color = (0, 0, 0)
+letter_color_correct = (0, 255, 0)
+letter_color_incorrect = (255, 0, 0)
 
 
-start_button = Button(text="Start", width=10)
 
-start_button_canvas = canvas.create_window(350, 400, anchor ="nw", window= start_button)
+while running:
+    clock.tick(60)
+    keys = pygame.key.get_pressed()
+    screen.blit(bg, (0,0))
+    if start_text_display:
+        start_text = draw_text("Press any key to start!", text_font, letter_color, 225,0)
+    else:
+        current_x = letter_pos_x
+        for i, letter in enumerate(letters_to_type):
+            if i < len(typed_letters):
+                if typed_letters[i] == letter:
+                    color = letter_color_correct
+                else:
+                    color = letter_color_incorrect
+            else:
+                color = letter_color
+            letter_width = draw_text(letter, text_font, color, current_x, letter_pos_y)
+            current_x += letter_width
+          
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if start_text_display:
+                start_text_display = False
+            else:
+                if event.key == pygame.K_BACKSPACE:
+                    if typed_letters:
+                        last_letter_width = draw_text(typed_letters.pop(), text_font, letter_color, 0, 0)
+                        letter_pos_x += last_letter_width
+                elif len(typed_letters) < len(letters_to_type):
+                    typed_letter = event.unicode
+                    typed_letters.append(typed_letter)
+                    last_letter_width = draw_text(typed_letter, text_font, letter_color, 0, 0)
+                    letter_pos_x -= last_letter_width           
+    
+    pygame.display.update()        
 
-root.mainloop()
+       
